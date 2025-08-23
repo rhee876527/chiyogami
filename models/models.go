eab7459af7524a775fe5e65b7e059ff5e10d4fb2
Martin Kibera <26869897+rhee876527@users.noreply.github.com>
2025-08-23 10:44:05 +0300

    index the DB

--- models/models.go
+++ models/models.go
@@ -8,17 +8,17 @@ import (
 
 type Paste struct {
 	gorm.Model
-	Title       string `gorm:"unique;size:4"`
-	Content     string `gorm:"type:text"`
-	Visibility  string
-	Expiration  *time.Time `json:"Expiration"`
-	IsEncrypted bool
-	UserID      uint
-	IsUserPaste bool
+	Title       string     `gorm:"uniqueIndex;size:4"`
+	Content     string     `gorm:"type:text"`
+	Visibility  string     `gorm:"index:vis_enc"`
+	Expiration  *time.Time `gorm:"index" json:"Expiration"`
+	IsEncrypted bool       `gorm:"index:vis_enc"`
+	UserID      uint       `gorm:"index:user_paste"`
+	IsUserPaste bool       `gorm:"index:user_paste"`
 }
 
 type User struct {
 	gorm.Model
-	Username string `gorm:"unique"`
+	Username string `gorm:"uniqueIndex"`
 	Password string
 }
